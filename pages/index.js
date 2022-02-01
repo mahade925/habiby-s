a94fe1ea664dd49408a72a8b39175b5185a18f3b
mahade925 <learnwithhasan2@gmail.com>
2022-02-01 13:32:21 +0600

    all error fixed

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
+import Script from 'next/script'
 
 const index = () => {
   return (
     <>
       <Head>
-        <title>Habiby's</title>
+        <title>Habiby-s</title>
         <link rel="shortcut icon" href="https://i.ibb.co/ZHL48R5/logo.png" />
-        <script src="https://cdn.tailwindcss.com"></script>
-        <link href="/dist/output.css" rel="stylesheet"></link>
+        {/* <link href="/dist/output.css" rel="stylesheet"></link> */}
       </Head>
+      <Script src="https://cdn.tailwindcss.com"></Script>
       <Navbar></Navbar>
       <HeroSection></HeroSection>
       <h1>.</h1>
